2681e023b265ac3d2455b1fd1ea80f4e25b29fa3
daffolap <shivam.mishra@daffodilsw.com>
2018-09-19 14:10:50 +0530

    .

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.use((req,res,next)=>{
 })
 app.use((error,req,res,next)=>{
     res.status(error.status || 500);
-    res.json({
-        error:{
-            message: error.message
-        }
-    })
+    res.send('Server Works')
 })
 module .exports = app;
